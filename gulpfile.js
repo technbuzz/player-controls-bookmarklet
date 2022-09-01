f6792d867f9d5ba6f7f4599facae8ac147cd7621
technbuzz <samiullah.khan.1989@gmail.com>
2022-09-01 14:56:02 +0400

    added docs for github page

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,25 @@
 const {dest, src, series} = require('gulp')
 const bookmarkify = require('gulp-bookmarkify');
-const inject = require('gulp-inject');
+const rename = require('gulp-rename');
+const uglify = require('gulp-uglify');
+const concat = require('gulp-concat');
 
 function defaultTask(cb) {
   src('src/bookmark.js')
   .pipe(bookmarkify())
+  .pipe(rename({ suffix: '.min'}))
   .pipe(dest('dist'))
   cb();
 }
 
-// function injectAnchorScript(cb) {
-//   src('src/index.html')
-//   .pipe( 
-//
-//
-// }
+function bundle(cb) {
+  src('src/*.js')
+  .pipe(concat('bundle.min.js'))
+  .pipe(uglify())
+  // .pipe(rename({ suffix: '.min'}))
+  .pipe(dest('docs')) 
 
-exports.default = series(defaultTask)
+  cb();
+}
+
+exports.default = series(defaultTask, bundle)
